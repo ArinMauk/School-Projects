75f51611156014f5e1a047327e1680977e4b93f1
Arin Mauk <arin.mauk7@gmail.com>
2018-05-03 19:37:42 -0400

    updated testClean dataset

--- SeniorProject/testcleanDataset.py
+++ SeniorProject/testcleanDataset.py
@@ -8,4 +8,6 @@ print x.organizeConversationData();
 #print x.getMovies('u0 +++$+++ u2 +++$+++ m9564 +++$+++ [\'L194\', \'L195\', \'L196\', \'L197\']')
 #print x.getUsers('u0 +++$+++ u2 +++$+++ m9564 +++$+++ [\'L194\', \'L195\', \'L196\', \'L197\']')
 #print x.getConversationData('u0 +++$+++ u2 +++$+++ m9564 +++$+++ [\'L194\', \'L195\', \'L196\', \'L197\']')
-print x.printConversationsMatrix();
+#print x.printConversationsMatrix();
+#x.fillInConversationData();
+x.findLineText("L924");
